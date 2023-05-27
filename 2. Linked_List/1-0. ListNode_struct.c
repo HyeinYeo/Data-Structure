0ddbfffb9bde9028cd173221f58ea51fdd11ad95
Gamza363 <100903181+HyeinYeo@users.noreply.github.com>
2023-05-27 15:51:14 +0900

    Delete 2. Linked_List directory

--- 2. Linked_List/1-0. ListNode_struct.c
+++
@@ -1,5 +0,0 @@
-/* 단순연결리스트 구조체 */
-typedef struct ListNode{
-	int data;
-	struct ListNode *link;
-}ListNode;
--- 2. Linked_List/3-0. DoublyList_struct.c
+++
@@ -1,5 +0,0 @@
-/*이중연결리스트 구조체*/
-typedef struct DListNode{
-	int data;
-	struct DListNode *left, *right;
-}DListNode;
